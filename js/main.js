d3a5c1006372a813899556e96e8fab3ed42cb04e
Arinzechukwu Okoye <maziarinze@icloud.com>
2021-11-26 02:06:11 -0600

    Custom mobile responsive design files added

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+// ...... Responsive Topnav ......
+
+$(document).ready(function(){
+
+    $nav = $(".nav");
+    $toggleCollapse = $(".toggle-collapse");
+
+    /* Click event on toggle menu */
+    $toggleCollapse.click(function(){
+        $nav.toggleClass("collapse");
+    })
+    
+    // Owl caroursel for blog
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$(".owl-navigation .owl-nav-prev"), $(".owl-navigation .owl-nav-next")]
+
+    });
+
+});
+
+
+/* Bar icon for navigation menu */
+
+
+
+// ...x... Responsive Topnav ...x...
